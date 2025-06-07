5cad6cd5fde7a88be920f1bb5606d62337290516
Daniil Sebiakin <sebyackindaniil@ya.ru>
2025-06-07 21:43:27 +0300

    Review Fix #1

--- src/main/java/ru/yandex/practicum/filmorate/service/UserRecommendationsService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/UserRecommendationsService.java
@@ -5,7 +5,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import ru.yandex.practicum.filmorate.exception.NotFoundException;
 import ru.yandex.practicum.filmorate.model.Film;
-import ru.yandex.practicum.filmorate.storage.FilmStorage;
 import ru.yandex.practicum.filmorate.storage.UserStorage;
 
 import java.util.*;
